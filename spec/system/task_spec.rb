d4dbf1d54eadf8c00c9cf74056f1d3bea790042b
hakubei <sho1018ta@icloud.com>
2021-03-08 22:05:31 +0900

    spec/factories/task.rbにassociationを記述

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -79,7 +79,7 @@ RSpec.describe 'Task', type: :system do
 
       it '既にステータスが完了のタスクのステータスを変更した場合、Taskの完了日が更新されないこと' do
         # TODO: FactoryBotのtraitを利用してください
-        visit edit_project_task_path(project, task_done )
+        visit edit_project_task_path(project, task_done)
         select 'todo', from: 'Status'
         click_button 'Update Task'
         expect(page).to have_content('todo')
