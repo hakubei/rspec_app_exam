d94a0b0c027e827e1ad6667b44300b28400aacec
hakubei <sho1018ta@icloud.com>
2021-03-09 00:23:25 +0900

    Taskが削除されること を修正

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -89,15 +89,16 @@ RSpec.describe 'Task', type: :system do
       end
     end
   end
-
+  
   describe 'Task削除' do
     context '正常系' do
+      let!(:task) { create(:task) }
       # FIXME: テストが失敗するので修正してください
       it 'Taskが削除されること' do
         visit project_tasks_path(project)
         click_link 'Destroy'
         page.driver.browser.switch_to.alert.accept
-        expect(page).not_to have_content task.title
+        expect(find('.task_list')).not_to have_content task.title
         expect(Task.count).to eq 0
         expect(current_path).to eq project_tasks_path(project)
       end
