2648bd0361119bf9a7d91a3309a153d0b5c2ecdb
hakubei <sho1018ta@icloud.com>
2021-03-09 11:40:04 +0900

    Fix: spec/system/task_spec.rb　taskのproject_idを削除

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Task', type: :system do
   
   let(:project) { create(:project) }
-  let(:task) { create(:task, project_id: project.id) }
+  let(:task) { create(:task) }
   let(:task_done) { create(:task, :done) }
 
   describe 'Task一覧' do
